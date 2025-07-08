221ff573bf8be5333438a9691edc009d1c1e484e
rogerting17 <76736455+rogerting17@users.noreply.github.com>
2025-07-08 19:51:56 +0800

    Delete frontend-cloudflare/js/update.js

--- frontend-cloudflare/js/update.js
+++
@@ -1,8 +0,0 @@
-function updateData() {
-  fetch("https://growth-api-be3i.onrender.com/update?token=Chip2150", {
-    method: "POST"
-  })
-  .then(res => res.json())
-  .then(data => alert(data.message || "✅ 更新完成"))
-  .catch(err => alert("❌ 更新失敗"));
-}
